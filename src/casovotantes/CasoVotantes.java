e2a7d9dd72ef9aef47771bcbeb8494c68b6fd254
stgoirh <stgoirh@SantiagoIRH>
2015-09-28 16:07:32 -0300

    V1.1 (falta diferenciar hombres de mujeres)

--- src/casovotantes/CasoVotantes.java
+++ src/casovotantes/CasoVotantes.java
@@ -15,17 +15,30 @@ public class CasoVotantes {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        char[] Votantes;
+        int[] Votantes;
+        int Cont0=0;
+        int Cont1=0;
+        int ContN=0;
         int N= (int)Math.floor(Math.random()*(100-500)+500);
-        Votantes=new char[N];
-        for(int i=0; i<N; i++){
-            Votantes[i]=()Math.random()*1;
+        Votantes=new int[N];
+        for(int i=0; i>=N; i++){
+            Votantes[i]=(int) (Math.random()*2);
         }
-        
-        
-            
-        
-        
+        for(int t=0; t>=N; t++){
+            if(Votantes[t]<1){
+                Votantes[t]=0;
+                Cont0=Cont0+1;
+            }else{
+                if(Votantes[t]>1){
+                    Votantes[t]=1;
+                    Cont1=Cont1+1;
+                }else{
+                    Votantes[t]=5;
+                    ContN=ContN+1;
+                }
+            }
+        }
+        System.out.println(Cont0+" Votantes");
     }
     
 }
