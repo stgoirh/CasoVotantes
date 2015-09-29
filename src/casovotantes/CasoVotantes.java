4d0c8bdf04cd8d5006bcac1d06456b97f184085c
stgoirh <stgoirh@SantiagoIRH>
2015-09-28 23:39:42 -0300

    V1.2 "Final"

--- src/casovotantes/CasoVotantes.java
+++ src/casovotantes/CasoVotantes.java
@@ -15,30 +15,51 @@ public class CasoVotantes {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        int[] Votantes;
-        int Cont0=0;
-        int Cont1=0;
-        int ContN=0;
+         int[] Votantes;
+        int Cont0H=0;
+        int Cont1H=0;
+        int ContNH=0;
+        int Cont0M=0;
+        int Cont1M=0;
+        int ContNM=0;
         int N= (int)Math.floor(Math.random()*(100-500)+500);
+        if (N%2!=0){
+            N=N+1;
+        }
         Votantes=new int[N];
-        for(int i=0; i>=N; i++){
-            Votantes[i]=(int) (Math.random()*2);
+        for(int i=0; i<N; i++){
+            Votantes[i]=(int) (Math.random()*4);
         }
-        for(int t=0; t>=N; t++){
-            if(Votantes[t]<1){
-                Votantes[t]=0;
-                Cont0=Cont0+1;
-            }else{
-                if(Votantes[t]>1){
-                    Votantes[t]=1;
-                    Cont1=Cont1+1;
+         for(int t=0; t<N; t++){//1.2.3.4.5    
+            if(t%2!=0){
+                if(Votantes[t]==1){
+                     Cont0M=Cont0M+1;
+            
+               }else if (Votantes[t]==2){
+                Cont1M=Cont1M+1;
                 }else{
-                    Votantes[t]=5;
-                    ContN=ContN+1;
+                ContNM=ContNM+1;
                 }
-            }
-        }
-        System.out.println(Cont0+" Votantes");
+            }else{
+
+               if(Votantes[t]==1){
+                  Cont0H=Cont0H+1;
+            
+               }else if (Votantes[t]==2){
+                 Cont1H=Cont1H+1;
+                }else{
+                    ContNH=ContNH+1;
+                }
+           }    
+}
+                
+        System.out.println(Cont1H+" numero de hombres que aprueban");
+        System.out.println(Cont0H+" numero de hombres que desaprueban");
+        System.out.println(ContNH+" numero de hombres que no sabe,no responde");
+        System.out.println(Cont1M+" numero de mujeres que aprueban");
+        System.out.println(Cont0M+" numero de mujeres que desaprueban");
+        System.out.println(ContNM+" numero de mujeres que no sabe,no responde");
+        System.out.println(Votantes.length);
     }
     
 }
